1db85c4596de023eb7b6a1d7cf1ceb497e18ac47
luisedu143 <112430715+luisedu143@users.noreply.github.com>
2023-11-09 11:23:56 -0300

    Add files via upload

---
+++ musica_sesc (1).sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 09-Nov-2023 às 15:20
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `musica`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `musica sesc`
+--
+
+CREATE TABLE `musica sesc` (
+  `genero` varchar(50) NOT NULL,
+  `ritimo` varchar(50) NOT NULL,
+  `cantor` varchar(50) NOT NULL,
+  `letra` varchar(50) NOT NULL,
+  `rima` varchar(50) NOT NULL,
+  `banda` varchar(50) NOT NULL,
+  `Auto-tune` varchar(50) NOT NULL,
+  `Presets` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
